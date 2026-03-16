14a7e7e3622f187f985c889b2bd1e64baaa49b83
alenalex005 <alenalex2405@gmail.com>
2026-03-16 22:10:37 +0530

    UC2 - Assignment - Print Banner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,8 +1,9 @@
 /**
  * OOPSBannerApp
- * This application displays "OOPS" (Object-Oriented Programming System) on the console.
+ * This application displays "OOPS" on the console as a literal string (UC1)
+ * and then renders it in a large banner format using ASCII art (UC2).
  * * @author Your Name
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
@@ -11,7 +12,31 @@ public class OOPSBannerApp {
      * * @param args Command line arguments
      */
     public static void main(String[] args) {
-        // Displays the string literal to the console
+
+        // ==========================================
+        // UC1: Print OOPS to Console
+        // ==========================================
+        System.out.println("--- UC1: Standard Print ---");
         System.out.println("OOPS");
+
+        System.out.println(); // Blank line for spacing
+
+        // ==========================================
+        // UC2: Render OOPS as Banner using Print Statements
+        // ==========================================
+        System.out.println("--- UC2: Banner Print ---");
+
+        // Rendering the OOPS Banner.
+        // Each letter is formatted to a 9-character width.
+        // String concatenation (+) combines O + O + P + S for each line.
+
+        System.out.println("  ***** " + " " + "  ***** " + " " + "******* " + " " + "  ***** ");
+        System.out.println(" * * " + " " + " * * " + " " + "* * " + " " + " * ");
+        System.out.println("* *" + " " + "* *" + " " + "* * " + " " + "* ");
+        System.out.println("* *" + " " + "* *" + " " + "******* " + " " + " ***** ");
+        System.out.println("* *" + " " + "* *" + " " + "* " + " " + "      * ");
+        System.out.println(" * * " + " " + " * * " + " " + "* " + " " + "       * ");
+        System.out.println("  ***** " + " " + "  ***** " + " " + "* " + " " + "  ***** ");
+
     }
 }
