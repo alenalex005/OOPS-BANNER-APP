c9a3343496a9a6c3d8499f92179a0adf7a67c7e8
alenalex005 <alenalex2405@gmail.com>
2026-03-16 22:01:10 +0530

    Initial Commit

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * This application displays "OOPS" (Object-Oriented Programming System) on the console.
+ * * @author Your Name
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The main method serves as the entry point for the application.
+     * * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // Displays the string literal to the console
+        System.out.println("OOPS");
+    }
+}
